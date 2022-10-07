6390e97f0ad5735ddc1e23d8370598c27d2299d6
Rjkzavr <kolya.toropof@gmail.com>
2022-10-07 17:25:45 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,44 @@
+import socket
+import json
+from time import time
+
+import select
+
+UDP_IP = "10.0.0.206"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+
+login_data = {"type": "login",
+              "nickname": input('Your nickname: ')}
+timee = time()
+sock.sendto(bytes(json.dumps(login_data), 'utf-8'), (UDP_IP, UDP_PORT))
+print(0)
+data, addr = sock.recvfrom(1024)
+print(1)
+request = json.loads(data)
+print(2)
+if request["status"] != "ok":
+    print(3)
+    print(request["error"])
+    input()
+    quit(1)
+else:
+    print(4)
+    print("Successful connection in", time() - timee)
+
+while True:
+    message = {"type": "message", "text": input('Message: ')}
+    sock.sendto(bytes(json.dumps(message), 'utf-8'), (UDP_IP, UDP_PORT))
+    have_data = True
+    while have_data:
+        timeout = 0.1  # wait in seconds
+        ready_sockets, _, _ = select.select(
+            [sock], [], [], timeout
+        )
+        if ready_sockets:  # work max 0.1 sec
+            data = sock.recv(1024)
+            print(data)
+        else:  # if work more
+            have_data = False
---
+++ for web.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import socket
+import json
+
+print(socket.gethostbyname(socket.gethostname()))
+UDP_IP = socket.gethostbyname(socket.gethostname())
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+users = {}
+
+while True:
+    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    request = json.loads(data)
+    if request["type"] == "login":
+        print(1)
+        if request["nickname"] not in users.values():
+            users[addr] = request["nickname"]
+            ans = {"type": "status", "error": "", "status": "ok"}
+            print(f"User {request['nickname']} successful connected")
+            sock.sendto(bytes(json.dumps(ans), 'utf-8'), addr)
+        else:
+            users[addr] = request["nickname"]
+            ans = {"type": "status", "error": "User already login", "status": "error"}
+            print(f"User {request['nickname']} error on connection")
+            sock.sendto(bytes(json.dumps(ans), 'utf-8'), addr)
+    elif request["type"] == "message":
+        print(2)
+        message = {"type": "message", "text": f"{users[addr]}: {request['text']}"}
+        for add in users.keys():
+            if addr != add:
+                sock.sendto(bytes(json.dumps(message), 'utf-8'), add)
